40ae7005b59e1bb42550b8ca3a45406bb7903b3c
Kenny Lowe <kenny.lowe@outlook.com>
2019-01-06 13:37:41 +0000

    Update test.sh to test for aberd process running

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-set -e
 
-cd /mud/bin
+# Start the first process
 ./aberd -k
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start my_first_process: $status"
+  exit $status
+fi
 
-cat
+while sleep 60; do
+  ps aux |grep aberd |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  # If the greps above find anything, they exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $PROCESS_1_STATUS -ne 0]; then
+    echo "aberd process has already exited."
+    exit 1
+  fi
+done
